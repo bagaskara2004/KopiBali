aceead76ad679564a7f038efe7f846c0b5ba2e80
Amee30 <darmaputra2004@gmail.com>
2024-06-03 12:52:51 +0800

    Update admin.php
    mengubah icon dan konten pada sidebar admin

--- app/Views/Admin/admin.php
+++ app/Views/Admin/admin.php
@@ -35,7 +35,6 @@
                 left: 0;
                 width: 100vw;
                 height: 100vh;
-                background-color: rgba(0, 0, 0, 0.5);
                 z-index: 999;
                 display: none;
             }
@@ -43,6 +42,7 @@
             .content-overlay-show {
                 display: block;
             }
+
         }
     </style>
 </head>
@@ -66,48 +66,24 @@
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
                         <a href="#" class="nav-link align-middle px-0">
-                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                            <i class="bi bi-clipboard-data-fill fs-3"></i> <span class="ms-1 d-none d-sm-inline">Overview</span>
                         </a>
                     </li>
                     <li>
                         <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
-                    </li>
-                    <li>
-                        <a href="#" data-bs-toggle="collapse" class="nav-link px-0 align-middle" data-bs-target="#submenu2" aria-expanded="false">
-                            <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span>
-                        </a>
-                        <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" data-bs-toggle="collapse" class="nav-link px-0 align-middle" data-bs-target="#submenu3" aria-expanded="false">
-                            <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span>
-                        </a>
-                        <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
-                            </li>
-                        </ul>
+                            <i class="bi bi-bag-fill fs-3"></i> <span class="ms-1 d-none d-sm-inline">Product</span></a>
                     </li>
                     <li>
                         <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span>
+                            <i class="bi bi-image-fill fs-3"></i> <span class="ms-1 d-none d-sm-inline">Galery</span></a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle">
+                            <i class="bi bi-megaphone-fill fs-3"></i> <span class="ms-1 d-none d-sm-inline">Promotion</span></a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle">
+                            <i class="bi bi-bell-fill fs-3"></i> <span class="ms-1 d-none d-sm-inline">Subciption</span>
                         </a>
                     </li>
                 </ul>
